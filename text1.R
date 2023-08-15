82687f8be78650576cd58c183348e3bf064a51dd
jianing724 <gaorong@nat-wireless-guest-reg-153-51.bu.edu>
2023-08-15 16:13:27 -0400

    name

---
+++ text1.R
@@ -0,0 +1,11 @@
+# text 1 script #
+
+# YOUR NAME HERE #
+
+#replace the following with your information
+
+
+w <- list("text1", # leave this alone
+          "Jenny", # enter your first name
+          "Yi", # enter your sur name
+          "U11235813") # enter your ID
